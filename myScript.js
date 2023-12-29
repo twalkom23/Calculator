6ab05bc7814f027c9cf9b9de351ba84d0934e7d6
Tim Walkom <timwalkom@hotmail.com>
2023-12-29 11:01:21 +1030

    Started creating the operating function

--- myScript.js
+++ myScript.js
@@ -1,3 +1,21 @@
+let one = '1';
+let two = '2';
+let three = '3';
+let four = '4';
+let five = '5'; 
+let six = '6';
+let seven = '7';
+let eight = '8';
+let nine = '9';
+let decimal = '.';
+let addition = '+'; 
+let times = '*';
+let minus = '-';
+let division = '/';
+let num1 ;
+let num2 ;
+let operater ;
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -10,5 +28,21 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1/num2;
 }
+
+//function taking the input from the buttons and then running the equation
+function operate(equation) {
+    let arrayEquation = equation.split(' '); //spliting the string in to an array
+    num1 = arrayEquation[0];
+    operater = arrayEquation[1];
+    num2 = arrayEquation[2];
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    
+}
+
+let test = operate('33 + 44');
+
+
+
 
 
