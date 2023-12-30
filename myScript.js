0ec324c6c7cf6c7f3ef6e40536befd209ea3d679
Tim Walkom <timwalkom@hotmail.com>
2023-12-31 08:18:51 +1030

    Added event listeners for all the numbers

--- myScript.js
+++ myScript.js
@@ -7,11 +7,12 @@ let six = '6';
 let seven = '7';
 let eight = '8';
 let nine = '9';
+let zero = '0';
 let decimal = '.';
-let addition = '+'; 
-let times = '*';
-let minus = '-';
-let division = '/';
+let addition = ' + '; 
+let times = ' * ';
+let minus = ' - ';
+let division = ' / ';
 let num1 ;
 let num2 ;
 let operator ;
@@ -46,11 +47,65 @@ function operate(equation) {
 
 }
 
-let test = operate('3.55 + 44');
+//This function will perform actions when buttons are clicked
+function calculatorInput() {
+    let buttonClickString = ''; //string that numbers will be added to to be passed in to operate function
 
+    const oneClick = document.querySelector('#one');
+    const twoClick = document.querySelector('#two');
+    const threeClick = document.querySelector('#three');
+    const fourClick = document.querySelector('#four');
+    const fiveClick = document.querySelector('#five');
+    const sixClick = document.querySelector('#six');
+    const sevenClick = document.querySelector('#seven');
+    const eightClick = document.querySelector('#eight');
+    const nineClick = document.querySelector('#nine');
+    const zeroClick = document.querySelector('#zero');
+    const decimalClick = document.querySelector('#decimal')
+    const divideClick = document.querySelector('#divide');
+    const clearClick = document.querySelector('#clear');
+    const multiplyClick = document.querySelector('#multiply');
+    const minusClick = document.querySelector('#minus');
+    const plusClick = document.querySelector('#plus');
+    const equalClick = document.querySelector('#equal');
+    
+    oneClick.addEventListener('click', () => {
+        buttonClickString = buttonClickString + one;
+        
+    })
+    twoClick.addEventListener('click', () => {
+        buttonClickString = buttonClickString + two;
+    })
+    threeClick.addEventListener('click', () => {
+        buttonClickString = buttonClickString + three;
+    })
+    fourClick.addEventListener('click', () => {
+        buttonClickString = buttonClickString + four;
+    })
+    fiveClick.addEventListener('click', () => {
+        buttonClickString = buttonClickString + five;
+    })      
+    sixClick.addEventListener('click', () => {
+        buttonClickString = buttonClickString + six;
+    })
+    sevenClick.addEventListener('click', () => {
+        buttonClickString = buttonClickString + seven;
+    })
+    eightClick.addEventListener('click', () => {
+        buttonClickString = buttonClickString + eight;
+    })
+    nineClick.addEventListener('click', () => {
+        buttonClickString = buttonClickString + nine;
+    })
+    zeroClick.addEventListener('click', () => {
+        buttonClickString = buttonClickString + zero;
+        
+    })
+    
+    
+}
 
-
-
+calculatorInput();
 
 
 
